2f9c14111a9113a099ad920b32fbf294ef71ff49
JUDHI3134 <judhistirbehera241@gmail.com>
2024-12-07 16:55:45 +0530

    display name in header section

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { assets } from '../assets/assets'
 import { useNavigate } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
 
 const Header = () => {
 
+    const {userData} = useContext(AppContext)
     const navigate = useNavigate()
+
 
   return (
     <div className='flex flex-col items-center mt-20 px-4 text-center text-gray-800'>
       <img src={assets.header_img} className='w-36 h-36 rounded-full mb-6' alt="" />
-      <h1 className='flex items-center gap-2 text-xl sm:text-3xl font-medium mb-2'>Hey Developer <img src={assets.hand_wave} className='w-8 aspect-square' alt="" /> </h1>
+      <h1 className='flex items-center gap-2 text-xl sm:text-3xl font-medium mb-2'>Hey {userData ? userData.name : "Developer"} <img src={assets.hand_wave} className='w-8 aspect-square' alt="" /> </h1>
       <h3 className='text-3xl sm:text-5xl font-semibold mb-4'>Welcome to our App</h3>
       <p className='mb-8 max-w-md'>Let's start with a quick product store and we will  have you up and running in no time!</p>
       <button onClick={()=>navigate("/login")} className='border border-gray-500 rounded-full px-8 py-2.5 hover:bg-gray-100 transition-all'>Get Started</button>
